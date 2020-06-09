afefd07277f4225f8a588619ff62be4b75ec6d04
Alex Kucherenko <kucherenko.email@gmail.com>
2020-06-10 00:26:30 +0300

    Project update

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -7,14 +7,14 @@ pub fn start(state: &State) {
 
   let mut assets = state.world.borrow::<UniqueViewMut<Assets>>();
 
-  assets.load_texture(0, "tree.png");
-  assets.load_texture(1, "cat.png");
+  //assets.load_texture(0, "tree.png");
+  //assets.load_texture(1, "cat.png");
 
   let entity_id = state.create_game_object();
   let entity_id2 = state.create_game_object();
 
   let (entities, mut textures) = state.world.borrow::<(EntitiesView, ViewMut<Texture>)>();
 
-  entities.add_component(&mut textures, Texture::new("tree.png", 32, 32), entity_id);
-  entities.add_component(&mut textures, Texture::new("cat.png", 32, 32), entity_id2);
+  //entities.add_component(&mut textures, Texture::new("tree.png", 32, 32), entity_id);
+  //entities.add_component(&mut textures, Texture::new("cat.png", 32, 32), entity_id2);
 }
