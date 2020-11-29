1e108724ea3f949511a34be41ac5a0b7a5fd3979
NikitaKucherenko <kucherenko.nikita2012@gmail.com>
2020-11-29 21:36:13 +0200

    added monsters

--- resources/tilesets/objects.tsx
+++ resources/tilesets/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="objects" tilewidth="82" tileheight="154" tilecount="3" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="objects" tilewidth="82" tileheight="154" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="82" height="154" source="../../assets/images/winter/objects/tree_1.png"/>
@@ -9,5 +9,14 @@
  </tile>
  <tile id="2">
   <image width="33" height="29" source="../../assets/images/winter/objects/box_1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="32" source="../../assets/images/winter/objects/Sprite-0001.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../assets/images/winter/objects/water-monster.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../assets/images/winter/objects/skeletens.png"/>
  </tile>
 </tileset>
